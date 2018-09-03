006b1f4407456b224b7c9a0fa4c054c59fe7c001
Ad_JA_cency <somwipaprasertsri@gmail.com>
2018-09-03 10:09:09 +0700

    home first

--- src/app/user/home-first/home-first.component.ts
+++ src/app/user/home-first/home-first.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Alert } from '../../domain/alert';
+import { AlertService } from '../../services/alertService';
 
 @Component({
   selector: 'app-home-first',
   templateUrl: './home-first.component.html',
-  styleUrls: ['./home-first.component.scss']
+  styleUrls: ['./home-first.component.scss'],
+  providers: [AlertService]
 })
 export class HomeFirstComponent implements OnInit {
 
-  constructor() { }
+  data: Alert[];
+
+  constructor(private alertService: AlertService) { }
 
   ngOnInit() {
+    this.alertService.getData().then(res => {
+      this.data = res.reverse();
+    });
   }
 
   scroll(el) {
